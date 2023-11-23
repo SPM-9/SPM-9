55d5c525b03a2d99e4b0bc90447dd022877dd744
嘉然今天吃向晚 <1483823709@qq.com>
2023-11-24 01:20:19 +0800

    User的DAO层封装

--- src/main/java/com/fxxkyiwangchangxue/yiwangnochangxue/dao/mapper/UserMapper.java
+++ src/main/java/com/fxxkyiwangchangxue/yiwangnochangxue/dao/mapper/UserMapper.java
@@ -8,10 +8,23 @@ import java.util.List;
 public interface UserMapper {
     /**
      * 查询所有用户
-     * @return List
+     * @return List 返回User列表
      */
     List<User> selectAll();
+
+    /**
+     * 按uid查询用户
+     * @param uid 用户的uid
+     * @return User 返回User对象，uid不存在则返回null
+     */
     User selectByUID(int uid);
+
+    /**
+     * 按用户名和密码查询用户
+     * @param userName 用户名
+     * @param password 密码
+     * @return User 返回User对象，登录失败返回null
+     */
     User selectByLogin(@Param("userName") String userName,
                        @Param("password") String password);
 }
