f851fbd7ff4b920c1841417e8ae44f30e1bebfe4
嘉然今天吃向晚 <1483823709@qq.com>
2023-12-02 05:03:25 +0800

    Commit上传、根据userId和taskId查询功能

---
+++ src/main/java/com/fxxkyiwangchangxue/yiwangnochangxue/dao/mapper/CommitMapper.java
@@ -0,0 +1,23 @@
+package com.fxxkyiwangchangxue.yiwangnochangxue.dao.mapper;
+
+import com.fxxkyiwangchangxue.yiwangnochangxue.entity.Commit;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface CommitMapper {
+    List<Commit> SelectAll();
+
+    Commit SelectById(int taskId);
+
+    Commit SelectFileById(int taskId);
+
+    Commit SelectByTaskIdUId(@Param("taskId") int taskId,
+                             @Param("uid") int uid);
+
+    void InsertCommit(@Param("fori_userId") int userId,
+                        @Param("fori_taskId") int taskId,
+                        @Param("body") String body,
+                        @Param("fileName") String fileName,
+                        @Param("file") byte[] file);
+}
---
+++ src/main/java/com/fxxkyiwangchangxue/yiwangnochangxue/service/studytask/UploadCommit.java
@@ -0,0 +1,70 @@
+package com.fxxkyiwangchangxue.yiwangnochangxue.service.studytask;
+
+import java.io.*;
+
+import com.fxxkyiwangchangxue.yiwangnochangxue.dao.SqlSessionFactoryUtils;
+import com.fxxkyiwangchangxue.yiwangnochangxue.dao.mapper.CommitMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import org.apache.commons.io.IOUtils;
+import org.apache.ibatis.session.SqlSession;
+
+@WebServlet(name = "UploadCommit", value = "/UploadCommit")
+@MultipartConfig
+public class UploadCommit extends HttpServlet {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset = UTF-8");
+        PrintWriter pw = resp.getWriter();
+
+        String userIdString = req.getParameter("userId");
+        String taskIdString = req.getParameter("taskId");
+        String body = req.getParameter("body");
+        Part filePart = req.getPart("file");
+        if (userIdString == null || taskIdString == null || body == null) {
+            resp.setStatus(500);
+            return;
+        }
+
+        int userId = Integer.parseInt(userIdString);
+        int taskId = Integer.parseInt(taskIdString);
+
+
+        String fileName = null;
+        byte[] file = null;
+        if (filePart != null) {
+            fileName = filePart.getSubmittedFileName();
+            InputStream is = filePart.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            IOUtils.copy(bis, baos);
+            file = baos.toByteArray();
+        }
+
+        SqlSession sqlSession = SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
+        CommitMapper commitMapper = sqlSession.getMapper(CommitMapper.class);
+
+        commitMapper.InsertCommit(userId, taskId, body, fileName, file);
+        sqlSession.commit();
+
+        sqlSession.close();
+        resp.setStatus(200);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
