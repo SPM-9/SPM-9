30b2bc963bbd7dd347df3323d6327d11174ab58e
嘉然今天吃向晚 <1483823709@qq.com>
2023-12-08 00:42:44 +0800

    资源上传功能，改了服务器地址

---
+++ src/main/java/com/fxxkyiwangchangxue/yiwangnochangxue/dao/mapper/ResourceMapper.java
@@ -0,0 +1,7 @@
+package com.fxxkyiwangchangxue.yiwangnochangxue.dao.mapper;
+
+import com.fxxkyiwangchangxue.yiwangnochangxue.entity.Resource;
+
+public interface ResourceMapper {
+    void insertResource(Resource resource);
+}
