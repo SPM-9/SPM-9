9ffc21aad446f84469f1fb5f1876da3655fab72f
嘉然今天吃向晚 <1483823709@qq.com>
2023-12-07 22:19:58 +0800

    教师登录

---
+++ src/main/java/com/fxxkyiwangchangxue/yiwangnochangxue/service/TeacherLogin.java
@@ -0,0 +1,64 @@
+package com.fxxkyiwangchangxue.yiwangnochangxue.service;
+
+import java.io.*;
+
+import com.fxxkyiwangchangxue.yiwangnochangxue.dao.SqlSessionFactoryUtils;
+import com.fxxkyiwangchangxue.yiwangnochangxue.dao.mapper.TeacherMapper;
+import com.fxxkyiwangchangxue.yiwangnochangxue.dao.mapper.UserMapper;
+import com.fxxkyiwangchangxue.yiwangnochangxue.entity.Teacher;
+import com.fxxkyiwangchangxue.yiwangnochangxue.entity.User;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import org.apache.ibatis.session.SqlSession;
+
+@WebServlet(name = "TeacherLogin", value = "/TeacherLogin")
+public class TeacherLogin extends HttpServlet {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset = UTF-8");
+        PrintWriter pw = resp.getWriter();
+
+        String userName = req.getParameter("userName");
+        String password = req.getParameter("password");
+        if (userName == null || password == null) {
+            resp.setStatus(500);
+            return;
+        }
+
+        Teacher teacher = new Teacher();
+        teacher.setUserName(userName);
+        teacher.setPassword(password);
+
+        SqlSession sqlSession = SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
+        TeacherMapper teacherMapper = sqlSession.getMapper(TeacherMapper.class);
+
+        Teacher loginTeacher = teacherMapper.selectByLogin(teacher);
+        if (loginTeacher == null) {
+            pw.print("false");
+        } else {
+            Gson gson = new Gson();
+            String json = gson.toJson(loginTeacher);
+            pw.print(json);
+        }
+
+        sqlSession.close();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
