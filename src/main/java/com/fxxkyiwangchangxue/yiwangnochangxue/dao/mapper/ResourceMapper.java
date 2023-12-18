285c7b397cd5c88c283c1f7dfe909b068ede4e64
wusir57 <13917789448@163.com>
2023-12-18 13:45:00 +0800

    GetNotification后端

--- src/main/java/com/fxxkyiwangchangxue/yiwangnochangxue/dao/mapper/ResourceMapper.java
+++ src/main/java/com/fxxkyiwangchangxue/yiwangnochangxue/dao/mapper/ResourceMapper.java
@@ -2,6 +2,9 @@ package com.fxxkyiwangchangxue.yiwangnochangxue.dao.mapper;
 
 import com.fxxkyiwangchangxue.yiwangnochangxue.entity.Resource;
 
+import java.util.List;
+
 public interface ResourceMapper {
     void insertResource(Resource resource);
+    List<Resource> getResource();
 }
